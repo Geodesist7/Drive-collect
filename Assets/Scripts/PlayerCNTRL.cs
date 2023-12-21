151fb9c7c8156cb14731c96a1f44e71f28e4eec6
Geodesist7 <stasmixstas2@gmail.com>
2023-12-21 16:34:01 +0200

    Fourth commit
    - Car appearance
    - Car controller

---
+++ Assets/Scripts/PlayerCNTRL.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private bool _moveRight, _moveLeft;
+
+    public void MoverR(bool moveRight)
+    {
+        _moveRight = moveRight;
+    }
+    
+    public void MoverL(bool moveLeft)
+    {
+        _moveLeft = moveLeft;
+    }
+    private void Update()
+    {
+        if (_moveRight) transform.Translate(0.03f, 0, 0);
+        if (_moveLeft) transform.Translate(-0.03f, 0, 0);
+    }
+}
